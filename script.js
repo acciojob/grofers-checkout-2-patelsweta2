81a9895edc90d19a0ca0fd24444002d1b5b90a4a
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2024-01-01 16:04:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+
+function calculateTotal() {
+	const prices = document.querySelectorAll('[data-ns-test="prices"]');
+
+	let totalPrice = 0;
+	prices.forEach(priceElement => {
+		totalPrice+=parseFloat(priceElement.textContent);
+	});
+
+	const newRow = document.createElement('tr');
+	const totalCell = document.createElement('td');
+	totalCell.setAttribute('data-ns-test', 'grandTotal');
+	totalCell.textContent = 'Total: ' + totalPrice.toFixed(2);
+	newRow.appendChild(totalCell);
+
+	const table = document.getElementById('groceryTable');
+	table.appendChild(newRow);
+}
+
+calculateTotal();
+
